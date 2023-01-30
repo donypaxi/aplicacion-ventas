492949bee36ff3e51dd458bee04b540f99a8ad93
grandony <trinchvr@gmail.com>
2023-01-30 14:01:20 -0500

    2do commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import imagen1 from '../src/assets/img/imagen1.jpg'
+export const App = () => {
+  return (
+    <>
+      <div>
+        <img src={imagen1} alt="" />
+        <h2>espacio de imagen</h2>
+      </div>
+      <h1>app principal</h1>
+
+    </>
+  )
+}
